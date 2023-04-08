60eeb03392ffd4b1911b3e45ac1d0adcff9a6e57
Mohamed-Magdy-fayed <mohamed.ahmed.sc2@gmail.com>
2023-04-08 03:40:36 +0200

    Revert "fixed an issue with the checkout session success link"
    This reverts commit b8b89ffa8df66c5f0c35a8f8d9c5906e97af7244.

--- src/pages/result.tsx
+++ src/pages/result.tsx
@@ -5,17 +5,32 @@ import Main from '@/components/Main'
 import Footer from '@/components/Footer'
 import { Button, Typography } from '@material-tailwind/react'
 import { useRouter } from 'next/router'
-import { HOST_URL } from '../config/index'
+import { HOST_URL } from '@/config'
+import Loading from '@/components/Loading'
+import { useEffect } from 'react'
+import useAuth from '@/hooks/useAuth'
+import { useAppSelector } from '@/store/hooks'
 
 export async function getServerSideProps({ query }: { query: { session_id: string } }) {
-    const data = await fetchGetJSON(`${HOST_URL}/api/checkout_sessions/${query.session_id}`)
+    const data = await fetchGetJSON(
+        query.session_id
+            ? `${HOST_URL}/api/checkout_sessions/${query.session_id}`
+            : '')
     return { props: { data } }
 }
 
 export default function result({ data }: any) {
 
+    const loading = useAppSelector((state) => state.loading.value)
+    const authChecked = useAuth()
+
+    useEffect(() => {
+        if (!authChecked) return
+    }, [authChecked])
+
     const router = useRouter()
     if (!data) return <div>failed to load</div>
+    if (loading) return <Loading />
 
     return (
         <div className="min-h-screen flex flex-col items-center">
